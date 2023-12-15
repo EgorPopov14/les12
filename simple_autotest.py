ad1862416a4ccbd7d2e182bc73066914834f3266
USER PC1 <egorp1351@gmail.com>
2023-12-15 17:59:13 +0300

    tests from les12

---
+++ simple_autotest.py
@@ -0,0 +1,10 @@
+assert 2>1
+assert 3>1
+assert 3==3
+from fustions import salary,a
+
+assert a('Max')=='Hello,Max','Hello who error'
+assert a('leo')=='hello,leo','hello who error'
+assert a('nikita')=='hello,nikita','hello who error'
+assert  salary(2,1)==4
+assert salary(2,2)==8
